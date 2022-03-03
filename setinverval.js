037ccbbcc5e06bcd0b2b37c5b407cfaeeab4f628
anamerasul <anamerasul@gmail.com>
2022-03-03 18:41:02 +0600

    setInterval and clearInterval with x++ and ++x

---
+++ setinverval.js
@@ -0,0 +1,15 @@
+console.log('first');
+// setInterval(()=>{
+//         console.log('tik tik')
+// },1000)
+
+let second =0;
+
+const timeId = setInterval(()=>{
+        // second++
+        console.log(++second)
+        if(second >15){
+                clearInterval(timeId)
+        }
+},1000)
+console.log('second');
