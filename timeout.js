278f2603e2e40a61ff9e7fec4d3e6f4694dd6510
anamerasul <anamerasul@gmail.com>
2022-03-03 18:01:19 +0600

    setTimeout simple Asynchronous JS using

---
+++ timeout.js
@@ -0,0 +1,18 @@
+function doSomething(){
+        console.log('I am codeing javascript')
+}
+// doSomething()
+console.log('First: ami age');
+
+console.log('second:I am second');
+// setTimeout(doSomething,3000);
+setTimeout(function(){
+        console.log('I am using vs')
+},5000);
+
+setTimeout(()=>{
+        console.log('i am exproing mdn article')
+},4000)
+console.log('third');
+console.log('fourth')
+// doSomething()
